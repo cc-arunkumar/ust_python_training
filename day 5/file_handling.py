c487eb6d3f87dc5bd2de76be2b676929634ac7d5
Sai-Vamsi-Kodavati <303371@ust.com>
2025-11-10 22:52:24 +0530

    sai_vamsi_kodavati initial commit day 1 to day 5

---
+++ day 5/file_handling.py
@@ -0,0 +1,30 @@
+# file = open('handle1.txt','r')
+# with open("handle1.txt","w") as file:
+#     file.write("Hello!World!\n")
+#     file.write("Hola! Anjan Kushal")
+#     file.close()
+
+# content = file.read()
+# print(content)
+# file.close()
+
+
+# with open("handle1.txt","r") as file:
+#     content = file.read()
+#     print(content)
+
+import os 
+# if os.path.exists('handle1.txt'):
+#     print("Exist")
+# else:
+#     print("Not Exist")
+
+# os.remove("handle1")
+# i=1
+# with open("handle1.txt") as file:
+#     for line in file:
+#         print("Line ",i,":",line.strip())
+#         i += 1
+
+with open("handle1.txt","a") as file:
+    file.write("\npgEhvBHi Anjan!")
