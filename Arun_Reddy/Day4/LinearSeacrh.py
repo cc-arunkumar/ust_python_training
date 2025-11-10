b21f592ceaf543b657257aff47d93bc9fdb24f78
cc-arunkumar <er.arunscope@gmail.com>
2025-11-10 16:04:37 +0530

    Delete Arun_Reddy directory

--- Arun_Reddy/Day4/LinearSeacrh.py
+++
@@ -1,5 +0,0 @@
-list1=[2,9,10,23,78,90]
-target=23
-for item in list1:
-    if item==target:
-        print("target element is :",item)
--- Arun_Reddy/Day5/FileOp.py
+++
@@ -1,9 +0,0 @@
-# writing to the file,if file doesnot exit it creates a new one 
-file=open("example.txt","w")
-file.write("Hello world")
-file.write(" File handling Concepts.......")
-#reading from teh file 
-file=open("example.txt","r")
-print(file.read())
-#closing the file 
-file.close()
