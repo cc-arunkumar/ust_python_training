e6d700f15997d73a3eb402dc4e950ba68887bd6e
cc-arunkumar <er.arunscope@gmail.com>
2025-11-10 15:26:41 +0530

    Delete day3 directory

--- day3/set/task2_unique_custm_email_filter.py
+++
@@ -1,9 +0,0 @@
-campaign_A = {"john@ust.com", "priya@ust.com", "amit@ust.com", "rahul@ust.com"}
-campaign_B = {"neha@ust.com", "amit@ust.com", "priya@ust.com", "meena@ust.com"}
-print(campaign_A.union(campaign_B))
-print(campaign_A.intersection(campaign_B))
-print(campaign_A.difference(campaign_B))
-
-unique_count = len(campaign_A.union(campaign_B))
-print("Count of unique elements:", unique_count)
-
