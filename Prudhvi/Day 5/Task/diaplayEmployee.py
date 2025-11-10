bcccc79fdd50032e63fad06008797897e2ad4bc0
ArjunJS08 <303391@ust.com>
2025-11-10 15:56:44 +0530

    changes

--- Prudhvi/Day 5/Task/diaplayEmployee.py
+++
@@ -1,8 +0,0 @@
-import employeemanagement
-
-def display_employees():
-    with open(employeemanagement.EMPLOYEE_FILE) as file:
-        lines = file.readlines()
-    # if not lines: print("No records."); return
-    for line in lines:
-        print(" | ".join(line.strip().split(",")))
