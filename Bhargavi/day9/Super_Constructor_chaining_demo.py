1f8983f7e7e07a7d04e565dfd68ed90144446cd0
BhargaviS1 <303456@ust.com>
2025-11-15 09:38:12 +0530

    Update Super_Constructor_chaining_demo.py

--- Bhargavi/day9/Super_Constructor_chaining_demo.py
+++ Bhargavi/day9/Super_Constructor_chaining_demo.py
@@ -28,12 +28,14 @@ class Tester(Employee):
     def perform_task(self):
         print(f"{self.name} (ID: {self.emp_id}) tests code on {self.platform} in {self.department} department.")
 
+#created list including my data
 emp_list = [
     Employee("Bhargavi", 101, "HR"),
     Developer("Meena", 103, "Engineering", "Python"),
     Tester("VEena", 104, "QA", "Selenium")
 ]
 
+#loop through the list
 for e in emp_list:
     e.perform_task()
     
@@ -41,3 +43,4 @@ for e in emp_list:
 # Bhargavi (ID: 101) works in HR department.
 # Meena (ID: 103) develops on Python in Engineering department.
 # VEena (ID: 104) tests code on Selenium in QA department.
+
