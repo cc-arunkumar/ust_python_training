dd63be8fee14d7661b05b6b7e1f8a94ad0b82aa7
BhargaviS1 <303456@ust.com>
2025-11-15 07:23:36 +0530

    day 9 Bhargavi

---
+++ day9/method_resolution_order.py
@@ -0,0 +1,30 @@
+#MRO stands for Method Resolution Order in Python.
+
+#class Employee
+class Employee:
+    def perform_task(self):
+        print("Emploee : The manager, developer, and tester together complete the project")
+
+#class Manger
+class Manager(Employee):
+    def perform_task(self):
+        print(" Manger : The manager manages the team")
+
+class Developer(Employee):
+    pass
+     
+class Tester(Employee):
+    def perform_task(self):
+        print( " Tester : The tester corrects the code")
+
+# Create objects
+emp = [Employee(), Manager(), Developer(), Tester()]
+
+# Loop through each employee and call perform_task
+for e in emp:
+    e.perform_task()
+
+# Emploee : The manager, developer, and tester together complete the project
+#  Manger : The manager manages the team
+# Emploee : The manager, developer, and tester together complete the project    
+#  Tester : The tester corrects the code
