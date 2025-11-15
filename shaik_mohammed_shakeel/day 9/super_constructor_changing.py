6e2524d64009c3844088c013f23a6d9ca99b57cf
Shaik-Mohammed-Shakeel <303397@ust.com>
2025-11-15 10:04:44 +0530

    Update super_constructor_changing.py

--- shaik_mohammed_shakeel/day 9/super_constructor_changing.py
+++ shaik_mohammed_shakeel/day 9/super_constructor_changing.py
@@ -4,26 +4,27 @@ class Employee:
         self.name=name
         self.dept=dept
 
-#calling Developer class
+# Developer class inherits from Employee
 
 class Developer(Employee):
     def __init__(self, id, name, dept,platform):
         super().__init__(id, name, dept)
         self.platform=platform
 
-#calling Frontend Developer
+# Frontend Developer Inherits from employee
 class FrontendDeveloper(Employee):
     def __init__(self, id, name, dept,technology):
         super().__init__(id, name, dept)
         self.technology=technology
 
-#calling Backend Developer
+# Backend Developer Inherits from Employee
 class BackendDeveloper(Employee):
     def __init__(self, id, name, dept,language):
         super().__init__(id, name, dept)
         self.language=language
         
 emp=Employee(101,"Shakeel","IT")
+# __dict__ shows all attributes of the object in dictionary form
 print(emp.__dict__)
 
 bkddev=BackendDeveloper(102,"Sameer","IT","Python")
@@ -31,4 +32,5 @@ print(bkddev.__dict__)
 
 #sample output
 # {'id': 101, 'name': 'Shakeel', 'dept': 'IT'}
+
 # {'id': 102, 'name': 'Sameer', 'dept': 'IT', 'language': 'Python'}
