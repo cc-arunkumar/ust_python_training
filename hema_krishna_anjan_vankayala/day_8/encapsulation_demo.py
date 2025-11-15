c511c9b0353567c730f6d961be1001eeed3c8fb4
anjan-ust <303376@ust.com>
2025-11-15 10:20:04 +0530

    Updated encapsulation_demo.py

--- hema_krishna_anjan_vankayala/day_8/encapsulation_demo.py
+++ hema_krishna_anjan_vankayala/day_8/encapsulation_demo.py
@@ -1,11 +1,15 @@
 #Employee Class
 class Employee:
     def __init__(self):
+         # Public attribute (can be accessed directly)
         self.name = "Anjan"
+
+        # Protected attribute (by convention, should not be accessed directly outside the class,
         self._designation = "SDE - 1"
+        # Private attribute (name mangling applied: stored internally as _Employee__salary)
         self.__salary = 80000
         
-    
+    #Show Method
     def show(self):
         print("Name:",self.name)
         print("Designation:",self._designation)
@@ -15,3 +19,4 @@ a1 = Employee()
 a1.show() # Acessing private vairable thorugh method
 print(a1._designation)
 print(a1._Employee__salary) # Accessing private variable wit mangling name
+
