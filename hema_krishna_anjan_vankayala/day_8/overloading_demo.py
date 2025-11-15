2addd06b404c8188c73fdda6456a68ec35b321a8
anjan-ust <303376@ust.com>
2025-11-15 10:14:51 +0530

    Updated overloading_demo.py

--- hema_krishna_anjan_vankayala/day_8/overloading_demo.py
+++ hema_krishna_anjan_vankayala/day_8/overloading_demo.py
@@ -1,12 +1,18 @@
+# Define a class
 class Test:
+    # First add method 
     def add(self,a,b=0):
         
         print("Sum of Two Numbers")
-    
+    # Second add method
     def add(self,a,b,c=0):
         print("Sum of Three Numbers")
-
+# Create an object of class Test
 m1 = Test()
 # m1.add(1) # Error
-m1.add(1,2)
-m1.add(1,2,3)
+
+# Call add method with two arguments
+m1.add(1,2)  # Output: "Sum of Three Numbers"
+
+# Call add method with three arguments
+m1.add(1,2,3) # # Output: "Sum of Three Numbers"
