7b820cf7a8cc8b6f32d581a5a403ad8bdd5a375f
anjan-ust <303376@ust.com>
2025-11-15 10:01:37 +0530

    Updated objects_features.py File

--- hema_krishna_anjan_vankayala/day_8/objects_features.py
+++ hema_krishna_anjan_vankayala/day_8/objects_features.py
@@ -1,26 +1,36 @@
 
 class Car:
+    # Constructor method to initialize
     def __init__(self,engine,fuel_injector):
         self.engine = engine
         self.fuel_injector = fuel_injector
-    
+        
+    # Method to add first set of accessories (comfort-related)
     def accessories1(self,ac,music,perfume):
         self.ac  = ac 
         self.music = music 
         self.perfume = perfume
-    
+        
+    # Method to add second set of accessories (safety-related)
     def accessories2(self,airbag,foglight,abs):
         self.airbag = airbag 
         self.foglight = foglight 
         self.abs = abs 
 
+# Create a Car object
 c1 = Car('Tata','suzuki')
 c1.accessories2('Sleepwell','Fogg','Ceat')
 print(c1.airbag)
 print(c1.abs)
 print(c1.foglight)
 print(c1.__dict__)
-   
+
+# output:
+# Sleepwell
+# Ceat
+# Fogg
+# { 'engine': 'Tata', 'fuel_injector': 'suzuki', 'airbag': 'Sleepwell', 'foglight': 'Fogg', 'abs': 'Ceat'
+# }
    
    
    
@@ -34,4 +44,5 @@ print(c1.__dict__)
     
 # n1 = Test()
 # n1.show()
+
 # print(n1.__dict__)      
