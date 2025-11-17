4552dca8501f9dd7b9ef2c38569800a1f60da83a
Akhil-UST-TVM <303394@ust.com>
2025-11-18 00:49:09 +0530

    akhil-day11 module task completed

---
+++ akhil_praveen/day11/module/module_demo.py
@@ -0,0 +1,34 @@
+# # from math import sqrt,pi,pow
+# import math
+# n=5
+# print(f"Square root of {n}: {math.sqrt(n)}")
+# print(f"value of pi: {math.pi}")
+# print(f"power: {math.pow(5,3)}")
+
+# n= 8.2
+
+
+# print(math.floor(n))
+# print(math.ceil(n))
+# print(math.trunc(n))
+# print(math.factorial(6))
+# print(math.fabs(-12))
+# print(math.log(n))
+# print(math.log10(n))
+# print(math.log2(n))
+# print(round(7.2))
+# print(round(7.8))
+# print(round(7.4))
+
+
+# import random
+
+# # print(random.randint(100000,999999))
+
+# fruit = ["apple","mango","kiwi","jack fruit","watermelon","orange","pineapple"]
+
+# print("Original fruit list =",fruit)
+# random.shuffle(fruit)
+# print("Randomly shuffled fruit list: ",fruit)
+
+# print("Random fruit : ",random.choice(fruit))
