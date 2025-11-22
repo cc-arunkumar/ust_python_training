67096f65c8f700bf91ddde0c2dbc83faf155833b
Akhil-UST-TVM <303394@ust.com>
2025-11-22 22:45:48 +0530

    task 30% complete

--- akhil_praveen/day15/assignment-ust_telecom/main.py
+++ akhil_praveen/day15/assignment-ust_telecom/main.py
@@ -3,16 +3,17 @@ from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel,Field
 import re
 
+app = FastAPI(title="Ust Employee Telecom Management")
 
-class Employee(BaseModel):
+class EmployeeBasic(BaseModel):
     emp_id : int = Field(...,ge=1000,le=999999,description="Employee id should be in between 1000 and 999999")
     name : str = Field(...,min_length=2,description="Length must be greater than or equal to 2")
     official_email : str = Field(...,pattern=r'^[a-zA-Z0-9._%+-]+@ust\.com$',description="Must match the pattern")
     department : Optional[str] = Field("General")
     location : Optional[str] = Field("Bangaluru")
     
-class Sim(BaseModel):
-    sim_number : str = Field(...,patter = r"^\d{10}$"description="Length should be exactly 10")
+class SimCard(BaseModel):
+    sim_number : str = Field(...,pattern = r"^\d{10}$",description="Length should be exactly 10")
     provider : Optional[str] = Field("Jio")
     is_esim : Optional[bool] = Field("False") 
     activation_year : int = Field(...,ge=2020,le=2025,description="Employee id should be in between 2020 and 2025")
@@ -21,11 +22,25 @@ class DataPlan(BaseModel):
     name : str = Field(...,min_length=3,max_length=50,description="Name should be in between 3 and 50 inclusive")
     monthly_gb : int = Field(...,gt=0,le=5000,description="Data volume should be less than 5000gb")
     speed_mbps :  Optional[int] = Field(...,50,ge=1,le=1000,description="Speed in mps should be less than 1000")
-    is_roaming_included : 
+    is_roaming_included : Optional[bool] = Field("False")
     
-class Register(BaseModel):
-    employee : Employee
-    sim : Sim
+class VoicePlan(BaseModel):
+    name : str = Field(...,min_length=3,description="Length must be greater than or equal to 3")
+    monthly_minutes : int = Field(...,ge=0,le=10000,description="Minutes should in between 0 and 10000")
+    has_isd : Optional[bool] = Field("False")
+    per_minute_charge_paise :  Optional[int] = Field(...,50,ge=1,le=1000,description="charge should be less than 1000")
+    
+class EmergencyContact(BaseModel):
+    name : str = Field(...,min_length=2,description="Length must be greater than or equal to 2")
+    relation : Optional[str] = Field("Family")
+    phone : str = Field(...,pattern = r"^[6-9]\d{9}$",description="Phone is not valid")
+    
+class EmployeeTelecomProfile(BaseModel):
+    employee : EmployeeBasic
+    sim : SimCard
+    data_plan : DataPlan
+    voice_plan : VoicePlan
+    emergency_contact : EmergencyContact
     
 data = {
  "employee": {
@@ -40,5 +55,5 @@ data = {
  }
  }
 
-obj = Register(employee=data["employee"],sim=data["sim"])
-print(obj.__dict__)
+@app.post("/telecom/profiles")
+def create_telecom_profile()
