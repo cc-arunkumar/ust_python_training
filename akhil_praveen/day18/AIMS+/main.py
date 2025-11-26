030f0547611cc5b819a3842f7a5058a3a214abaa
Akhil-UST-TVM <303394@ust.com>
2025-11-27 00:13:50 +0530

    initial commit of aims+

---
+++ akhil_praveen/day18/AIMS+/main.py
@@ -0,0 +1,76 @@
+from src.models.assetsinventory import AssetInventory
+from src.models.employeedirectory import EmployeeDirectory
+from src.models.maintainancelog import MaintenanceLog
+from src.models.vendormaster import VendorMaster
+ 
+import csv
+ 
+path = "D:/ust_python_training-1/arjun_j_s/day_18/AIMS_Plus/database/sample_data/"
+ 
+with open(path + "asset_inventory.csv", "r") as asset_file:
+    with open(path + "final/asset_inventory.csv", "w", newline="") as new_asset_file:
+        asset_data = csv.DictReader(asset_file)
+        header = asset_data.fieldnames
+        new_asset = csv.DictWriter(new_asset_file, header)
+        new_asset.writeheader()
+ 
+        for data in asset_data:
+            try:
+                # Validate row against model
+                asset = AssetInventory(**data)
+                new_asset.writerow(data)
+            except Exception as e:
+                print(f"Error: {e}")
+ 
+with open(path + "employee_directory.csv", "r") as emp_file:
+    with open(path + "final/employee_directory.csv", "w", newline="") as new_emp_file:
+        emp_data = csv.DictReader(emp_file)
+        header = emp_data.fieldnames
+        new_emp = csv.DictWriter(new_emp_file, header)
+        new_emp.writeheader()
+ 
+        for data in emp_data:
+            try:
+                # Validate row against model
+                emp = EmployeeDirectory(**data)
+                new_emp.writerow(data)
+            except Exception as e:
+                print(f"Error: {e}")
+                print("Unable to process record")
+ 
+with open(path + "maintenance_log.csv", "r") as maintain_file:
+    with open(path + "final/maintenance_log.csv", "w", newline="") as new_maintain_file:
+        maintain_data = csv.DictReader(maintain_file)
+        header = maintain_data.fieldnames
+        new_maintain = csv.DictWriter(new_maintain_file, header)
+        new_maintain.writeheader()
+ 
+        for data in maintain_data:
+            try:
+                # Validate row against model
+                emp = MaintenanceLog(**data)
+                new_maintain.writerow(data)
+            except Exception as e:
+                print(f"Error: {e}")
+                print("Unable to process record")
+ 
+ 
+with open(path + "vendor_master.csv", "r") as vendor_file:
+    with open(path + "final/vendor_master.csv", "w", newline="") as new_vendor_file:
+        vendor_data = csv.DictReader(vendor_file)
+        header = vendor_data.fieldnames
+        new_vendor = csv.DictWriter(new_vendor_file, header)
+        new_vendor.writeheader()
+ 
+        for data in vendor_data:
+            try:
+                # Validate row against model
+                emp = VendorMaster(**data)
+                new_vendor.writerow(data)
+            except Exception as e:
+                print(f"Error: {e}")
+                print("Unable to process record")
+ 
+ 
+ 
+ 
---
+++ akhil_praveen/day18/AIMS+/src/models/assetsinventory.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel,Field,model_validator
+from datetime import date
+from typing import Optional,Literal
+ 
+class AssetInventory(BaseModel):
+    asset_tag: str = Field(..., pattern=r"^UST-[A-Za-z0-9-]+$")
+    asset_type: Literal['Laptop', 'Monitor', 'Keyboard', 'Mouse']
+    serial_number: str
+    manufacturer: Literal['Dell', 'HP', 'Lenovo', 'Samsung','LG']
+    model: str
+    purchase_date: date
+    warranty_years: int = Field(..., ge=1, le=5)
+    condition_status: Literal['New', 'Good', 'Used', 'Damaged']
+    assigned_to: Optional[str] = None
+    location: Literal['TVM','Trivandrum','Bangalore', 'Chennai', 'Hyderabad']
+    asset_status: Literal['Available', 'Assigned', 'Repair', 'Retired']
+ 
+    @model_validator(mode="after")
+    def validate_purchase_date(self):
+        if self.purchase_date > date.today():
+            raise ValueError("purchase_date cannot be in the future")
+        return self
+ 
+ 
+ 
