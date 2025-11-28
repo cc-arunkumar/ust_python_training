edcb4bf750f711a944cf71ff9c2b886931583d5b
sohan-kumar-rout20 <303373@ust.com>
2025-11-28 16:29:19 +0530

    Added AIMS Plus task

--- Sohan-Kumar-Rout/day-18/AIMS+/src/config/db_connection.py
+++
@@ -1,8 +0,0 @@
-import pymysql
-def get_connection():
-    return pymysql.connect(
-        host="localhost",
-        user="root",
-        password="pass@word1",
-        database="ust_asset_db"
-    )
