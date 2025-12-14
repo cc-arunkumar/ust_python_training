a59dffe8a9e82367be1cf67ed41a8e06f071fca5
sohan-kumar-rout20 <303373@ust.com>
2025-12-14 14:39:48 +0530

    updated tasks

--- Sohan-Kumar-Rout/day-27/props-app/src/EmployeeCard.jsx
+++ Sohan-Kumar-Rout/day-27/props-app/src/EmployeeCard.jsx
@@ -4,7 +4,6 @@ const EmployeeCard = ({name,employeeid,role,location}) => {
   return (
     <div className='employee-card'>
         <h2>Employee Card</h2>
-        
         <p>Name : {name}</p>
         <p>EmployeeID : {employeeid}</p>
         <p>Role : {role}</p>
